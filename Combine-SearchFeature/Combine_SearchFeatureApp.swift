afca9503fe1217fd8971249ccf89e9a108775634
Christian Ray Leovido <christian.leovido@gmail.com>
2021-08-04 17:31:26 +0100

    Initial Commit

---
+++ Combine-SearchFeature/Combine_SearchFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_SearchFeatureApp.swift
+//  Combine-SearchFeature
+//
+//  Created by Christian Leovido on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Combine_SearchFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
